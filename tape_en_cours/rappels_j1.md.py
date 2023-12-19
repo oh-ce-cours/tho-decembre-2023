206e97336324ffcd52c34d48061e110aece9d4e9
Matthieu Falce <matthieu@falce.net>
2023-12-19 09:11:22 +0100

    gitautosync update 21

--- tape_en_cours/rappels_j1.md.py
+++ tape_en_cours/rappels_j1.md.py
@@ -1,6 +1,13 @@
 # Rappels du jour 1
 
+* 2 parties dans l'écriture d'un programme
+    * trouver l algorithme
+    * le traduire en python 
 * variables
+    * entiers
+    * flottants (nombres à virgule)
+    * chaines
+    * les listes 
 * les boucles
     * for
-    * whiles 
+    * while
