10478e9cc3a1a768bf54f3322ab982e4250777c6
Matthieu Falce <matthieu@falce.net>
2023-12-19 10:29:11 +0100

    gitautosync update 47

--- tape_en_cours/min_max.py
+++ tape_en_cours/min_max.py
@@ -1,4 +1,4 @@
-ma_liste = [5, 3, 2, 6, 4]
+ma_liste = [5, 3, 2, 6, 4, 1, 8]
 
 min_courant = ma_liste[0]
 max_courant = ma_liste[0]
@@ -8,4 +8,5 @@ for valeur in ma_liste:
         min_courant = valeur
     if max_courant < valeur:
         max_courant = valeur
+        
 print(min_courant, max_courant)    
