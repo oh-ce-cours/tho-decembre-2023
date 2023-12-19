29d048f4cbfa4dfd7f932b1a3feab76a01378532
Matthieu Falce <matthieu@falce.net>
2023-12-19 16:14:20 +0100

    gitautosync update 139

--- tape_en_cours/ouverture_csv.py
+++ tape_en_cours/ouverture_csv.py
@@ -9,6 +9,10 @@ df.dropna(axis=1, how='all', inplace=True)  # Supprimer les colonnes vides résu
 
 infos_train_1 = df[df["Numero de train"] == 1]
 
+# regrouper et faire des stats par train
 for num_train, grouped in df.groupby("Numero de train"):
     print(num_train, grouped.Valeur1.mean())
     
+
+# n'affiher que les données après un certain temps
+df[df.Timestamp > "2023-12-21 14:13:00"]
