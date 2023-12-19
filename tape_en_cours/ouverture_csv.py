ced29e7d9bfa6cfc87cf5df8fc9b411caa997b2a
Matthieu Falce <matthieu@falce.net>
2023-12-19 16:14:44 +0100

    gitautosync update 141

--- tape_en_cours/ouverture_csv.py
+++ tape_en_cours/ouverture_csv.py
@@ -1,7 +1,6 @@
 import pandas as pd
 
 df = pd.read_csv('demo_excel.csv', delimiter=";", decimal=",", parse_dates={'Timestamp': ['Date', 'Heure']}, dayfirst=True)
-#print(df.Valeur1.describe())
 df.dropna(axis=1, how='all', inplace=True)  # Supprimer les colonnes vides résultant du double point-virgule
 
 infos_train_1 = df[df["Numero de train"] == 1]
@@ -13,3 +12,6 @@ for num_train, grouped in df.groupby("Numero de train"):
 
 # n'affiher que les données après un certain temps
 df[df.Timestamp > "2023-12-21 14:13:00"]
+
+# description du dataframe
+print(df.Valeur1.describe())
