034f28145ada306ad0a5cfe1d90df44976b72f16
Matthieu Falce <matthieu@falce.net>
2023-12-19 17:12:05 +0100

    gitautosync update 166

--- tape_en_cours/fizzbuzz_fonctions.py
+++ tape_en_cours/fizzbuzz_fonctions.py
@@ -1,16 +1,15 @@
 def etape_fizz_buzz_1(nombre):
+    res = ""
+    if nombre % 3 == 0:
+        res = res + "fizz"
+    if nombre % 5 == 0:
+        res = res + "buzz"
+    if res == "":
+        res = str(nombre)
     return res 
 
 def fizz_buzz1(debut, fin):
     for nombre in range(debut, fin):
-        res = ""
-        if nombre % 3 == 0:
-            res = res + "fizz"
-        if nombre % 5 == 0:
-            res = res + "buzz"
-        if res == "":
-            res = str(nombre)
-        print(res)
     
 def fizz_buzz2(debut, fin):
     for nombre in range(debut, fin):
