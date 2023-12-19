630666f0519ad6124d2834c5e9c32edb6b809b5f
Matthieu Falce <matthieu@falce.net>
2023-12-19 14:12:51 +0100

    gitautosync update 80

--- tape_en_cours/rappel_j2.md.py
+++ tape_en_cours/rappel_j2.md.py
@@ -1,5 +1,19 @@
 # Rappel (2eme jour après-midi)
 
-Manipulations de boucles :
+Manipulations et mise en pratique de boucles :
     * `for`quand on connait le nombre de pas que l'on va faire
-    * `while`quand on ne le connait pas 
+    * `while`quand on ne le connait pas
+    
+Manipulation de conditions avec if
+    * if CONDITION:
+    * (elif CONDITION:) (optionnel, autant qu'on veut, permet de gérer les cas sinon si)
+    * (else:) (optionnel, on peut en mettre qu'un seul)
+        * on permet de capturer tous les autres cas
+    
+    
+    
+
+ma_liste = []
+    if ma_liste:
+<=> if len(ma_liste) > 0:
+    
