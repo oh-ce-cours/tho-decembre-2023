86a1b1df804afc304c727b6ad08dc61058c754de
Matthieu Falce <matthieu@falce.net>
2023-12-19 12:26:51 +0100

    gitautosync update 70

--- tape_en_cours/fizzbuzz.py
+++ tape_en_cours/fizzbuzz.py
@@ -1,9 +1,18 @@
 for nombre in range(1, 101):
-    res = ""
-    if nombre % 3 == 0:
-        res = res + "fizz"
-    if nombre % 5 == 0:
-        res = res + "buzz"
-    if res == "":
-        res = str(nombre)
-    print(res)        
+#     res = ""
+#     if nombre % 3 == 0:
+#         res = res + "fizz"
+#     if nombre % 5 == 0:
+#         res = res + "buzz"
+#     if res == "":
+#         res = str(nombre)
+#     print(res)        
+    divisible_par_5 = nombre % 5 == 0
+    divisible_par_3 = nombre % 3 == 0
+    
+    match divisible_par_3, divisible_par_5:
+        case True, True:
+            print("fizzbuzz")
+        case False, True:
+            print("fizzbuzz")
+
