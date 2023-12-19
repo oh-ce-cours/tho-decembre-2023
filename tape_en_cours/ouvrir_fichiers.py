8d8654ab65a939438a02cf0ee1abf2b220bcf753
Matthieu Falce <matthieu@falce.net>
2023-12-19 14:46:14 +0100

    gitautosync update 107

--- tape_en_cours/ouvrir_fichiers.py
+++ tape_en_cours/ouvrir_fichiers.py
@@ -1,5 +1,7 @@
 # ouverture en lecture 
 f = open("rappel_j2.md.py")
+f = open("C://Documents/pro/projet/file.txt")  # ouverture en mode absolue
+f = open("../medias/phrases.txt")
 
 # on lit le contenu et on le mets à jour en rajoutant une ligne à la fin
 file_content = f.readlines()
