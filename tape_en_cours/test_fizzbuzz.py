4f243afc4081fd24b16ff5f8136eadab98195d74
Matthieu Falce <matthieu@falce.net>
2023-12-19 17:23:37 +0100

    gitautosync update 177

---
+++ tape_en_cours/test_fizzbuzz.py
@@ -0,0 +1,17 @@
+def est_divisible_par(nombre, modulo):
+    return nombre / modulo == int(nombre / modulo)
+
+
+def etape_fizz_buzz_1(nombre):
+    res = ""
+    if est_divisible_par(nombre, 3):
+        res = res + "fizz"
+    if est_divisible_par(nombre, 5):
+        res = res + "buzz"
+    if res == "":
+        res = str(nombre)
+    return res 
+
+def test_est_divisible_par():
+    assert est_divisible_par(2, 2) == True
+    assert est_divisible_par(3, 2) == False
