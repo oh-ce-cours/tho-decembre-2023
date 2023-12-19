e33205b2e2279db0598b4bceb3fe0535c9cb4c04
Matthieu Falce <matthieu@falce.net>
2023-12-19 16:03:19 +0100

    gitautosync update 135

--- tape_en_cours/ouverture_csv.py
+++ tape_en_cours/ouverture_csv.py
@@ -1,7 +1,7 @@
 import pandas as pd
 
 df = pd.read_csv('demo_excel.csv', delimiter=";", decimal=",")
-print(df.Valeur1.describe())
+#print(df.Valeur1.describe())
 
 infos_train_1 = df[df["Numero de train"] == 1]
 
