157957cf28eb3960be34c25a55db69b2a85c570e
Matthieu Falce <matthieu@falce.net>
2023-12-19 15:21:42 +0100

    gitautosync update 122

--- tape_en_cours/magic_8_ball.py
+++ tape_en_cours/magic_8_ball.py
@@ -1,3 +1,5 @@
+import random
+
 f = open("../supports/medias/phrases_magic_8_ball.txt")  # ouverture en mode relatif
 
 phrases_magic_8_ball = []
@@ -5,3 +7,6 @@ phrases_magic_8_ball = []
 for line in f:
     if line != "\n":
         phrases_magic_8_ball.append(line)
+    
+
+print(random.choice(phrases_magic_8_ball))
