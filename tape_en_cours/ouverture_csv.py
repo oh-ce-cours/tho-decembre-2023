07e0ebc4c84e6b95220036399780ed37f81f1491
Matthieu Falce <matthieu@falce.net>
2023-12-19 16:10:43 +0100

    gitautosync update 136

--- tape_en_cours/ouverture_csv.py
+++ tape_en_cours/ouverture_csv.py
@@ -3,6 +3,10 @@ import pandas as pd
 df = pd.read_csv('demo_excel.csv', delimiter=";", decimal=",")
 #print(df.Valeur1.describe())
 
+# chat gpt
+df = pd.read_csv(csv_file, delimiter=';', decimal='.', parse_dates={'Timestamp': ['Date', 'Heure']}, dayfirst=True)
+df.dropna(axis=1, how='all', inplace=True)  # Supprimer les colonnes vides résultant du double point-virgule
+
 infos_train_1 = df[df["Numero de train"] == 1]
 
 for num_train, grouped in df.groupby("Numero de train"):
