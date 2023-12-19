f16209fdf04d6e66e3c0598cb06eec1b8b7db862
Matthieu Falce <matthieu@falce.net>
2023-12-19 14:47:32 +0100

    gitautosync update 111

--- tape_en_cours/ouvrir_fichiers.py
+++ tape_en_cours/ouvrir_fichiers.py
@@ -8,7 +8,7 @@ file_content = f.readlines()
 new_line = "Rajouté depuis Python :) \n"
 file_content.append(new_line)
 
-# ouverture en écriture (on mets à jour le fichier)
-f_ecriture = open("rappel_j2.md.py", mode="w")
-f_ecriture.writelines(file_content)
-f_ecriture.close()
+# # ouverture en écriture (on mets à jour le fichier)
+# f_ecriture = open("rappel_j2.md.py", mode="w")
+# f_ecriture.writelines(file_content)
+# f_ecriture.close()
